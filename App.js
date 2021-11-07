339c20fedbbb0473f5efe1e8f43965937647f765
Romeao <mandeep1lamsal@gmail.com>
2021-11-08 02:08:37 +0930

    build test

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import 'react-native-gesture-handler';
-//Always keep this in top
 
+import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
